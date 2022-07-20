a855f514bbc2b0dd3b378422c635fa1d448df0cc
Philip <6544880@mail.ru>
2022-07-20 18:39:57 +0300

    Commit#4: I want to make transfer information of selected book to page of each book

--- coolsite/phil/urls.py
+++ coolsite/phil/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='phil_home'),
-    path('books/', BookClass.as_view(), name='books'),
-    path('book/<slug:book_slug>/', book, name='book'),
+    path('books/', BooksList.as_view(), name='books'),
+    path('book/<slug:book_slug>/', ShowBook.as_view(), name='book'),
+#     path('book/<slug:book_slug>/', ShowBook.as_view(), name='book'),
     path('about/', about, name='phil_about'),
     path('login/', login, name='phil_login'),
     path('addbook', add_book, name='add_book'),
--- coolsite/phil/views.py
+++ coolsite/phil/views.py
@@ -10,17 +10,21 @@ from .forms import *
 # backup copy 18 July 2022 'Commit #3: creating form for adding book (it's working after a lot of fail attempts)'
 
 def index(request):
-
-#     return HttpResponse('<h1>Phil app’s main page</h1>')
     context = {
         'title': 'Страница Филиппа Назаренко',
     }
     return render(request, 'phil/index.html', context=context)
 
-class BookClass(ListView):
+class BooksList(ListView):
     model = Books
     template_name = 'phil/books.html'
-    extra_context = {'title': 'Прочитанные мною книги'}
+    extra_context = {'title': 'Прочитанные книги'}
+
+class ShowBook(DetailView):
+    model = Books
+    template_name = 'phil/book.html'
+    slug_url_kwarg = 'book_slug'
+    context_object_name = 'book'
 
 
 
@@ -39,61 +43,14 @@ def book(request, book_slug):
     return render(request, 'phil/book.html', context=context)
 
 def add_book(request):
-#    form = AddBookForm   # from file'forms.py'
    if request.method == 'POST':
-       form = AddBookForm(request.POST)
+       form = AddBookForm(request.POST, request.FILES)
        if form.is_valid():
-           # print(form.cleaned_data)
-           try:
-               Books.objects.create(**form.cleaned_data)
-               return redirect('books')
-           except:
-               form.add_error(None, 'Ошибка добавления книги')
+           form.save()
+           return redirect('books')
    else:
        form = AddBookForm()
    return render(request, 'phil/addbook.html', {'form': form, 'title': 'Добавить книгу'})
-
-def add_book(request):
-#    form = AddBookForm   # from file'forms.py'
-   if request.method == 'POST':
-       form = AddBookForm(request.POST)
-       if form.is_valid():
-           # print(form.cleaned_data)
-           try:
-               Books.objects.create(**form.cleaned_data)
-               return redirect('books')
-           except:
-               form.add_error(None, 'Ошибка добавления книги')
-   else:
-       form = AddBookForm()
-   return render(request, 'phil/addbook.html', {'form': form, 'title': 'Добавить книгу'})
-# #    form = AddBookForm()   # from file'forms.py'
-#     if request.method == 'POST':
-#         form = AddBookForm(request.POST)
-#         if form.is_valid():
-#             #print(form.cleaned_data)
-#             try:
-#                 Books.objects.create(**form.cleaned_data)
-#                 return redirect('books')
-#             except:
-#                 form.add_error(None, 'Ошибка добавления книги')
-#     else:
-#         form = AddBookForm()
-#     return render(request, 'phil/addbook.html', {'form': form, 'title': 'Добавить книгу'})
-
-
-
-# class ShowBook(DetailView):
-#     model = Books
-#     template_name = 'phil/book.html'
-#     slug_url_kwarg = 'book_slug'
-#     context_object_name = 'post'
-
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = context['post']
-    #     context['post_left'] = 'post_left'
-    #     return context
 
 
 def about(request):
@@ -102,11 +59,3 @@ def about(request):
 def login(request):
      return HttpResponse('<h1>Phil app’s login page</h1>')
 
-# In file 'views.py' in app 'phil' added comment: '# 08 may 2022y maked commit 'Commit #2 start';
-# Maked top menu, there added contacts, youtube logo, menu points, logo and it fixed on top;
-# Main page empty;
-# Page 'Прочитанные книги' make list of books: photo, author's description (take from DB), under two buttons: 'Моё описание и цитаты' & 'Описание книги (автор или издательство)', if push button transition to simple page with text: 'Author / publishing house description of book
-# 1'
-# Page 'women' available at 'http://127.0.0.1:8000/women/';
-# Сhapter 'women' made completely;
-# In database tables: 'phil_books', 'phil_quotes', 'women_categoty' & 'women_women';
